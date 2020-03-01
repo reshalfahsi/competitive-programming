5a9857d04ede80af4c075082313f930d946e69f6
Resha Dwika Hefni Al-Fahsi <resha.dwika.hefni.alfahsi@mail.ugm.ac.id>
2020-03-01 17:37:44 +0700

    update leaderboard

---
+++ ClimbingtheLeaderboard/climb_tlb.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the climbingLeaderboard function below.
+vector<int> climbingLeaderboard(vector<int> scores, vector<int> alice) {
+
+    vector<int> result;
+    sort(scores.begin(),scores.end());
+    auto new_scores = unique(scores.begin(), scores.end());
+    scores.erase(new_scores, scores.end());
+    auto size = alice.size();
+    auto N = scores.size();
+    std::vector<int>::iterator upper;
+    int pos;
+
+    for(int i = 0; i < size; i++){
+        if(alice[i] >= scores[N-1]) { pos = N; }
+        else{
+            upper = upper_bound(scores.begin(),scores.end(),alice[i]);
+            pos = upper - scores.begin();
+        } 
+        result.push_back((N-pos)+1);
+    }
+
+    return result;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int scores_count;
+    cin >> scores_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string scores_temp_temp;
+    getline(cin, scores_temp_temp);
+
+    vector<string> scores_temp = split_string(scores_temp_temp);
+
+    vector<int> scores(scores_count);
+
+    for (int i = 0; i < scores_count; i++) {
+        int scores_item = stoi(scores_temp[i]);
+
+        scores[i] = scores_item;
+    }
+
+    int alice_count;
+    cin >> alice_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string alice_temp_temp;
+    getline(cin, alice_temp_temp);
+
+    vector<string> alice_temp = split_string(alice_temp_temp);
+
+    vector<int> alice(alice_count);
+
+    for (int i = 0; i < alice_count; i++) {
+        int alice_item = stoi(alice_temp[i]);
+
+        alice[i] = alice_item;
+    }
+
+    vector<int> result = climbingLeaderboard(scores, alice);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
