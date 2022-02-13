cd83dae2b45fa379f8a9e39c34254891e4d1dde6
reshalfahsi <resha.alfahsi@gmail.com>
2022-02-13 12:50:54 +0700

    update BotClean Stochastic

--- cpp/BotCleanStochastic/botcleanstochastic.cpp
+++ cpp/BotCleanStochastic/botcleanstochastic.cpp
@@ -6,8 +6,6 @@
 using namespace std;
 
 #define N 5
-// #define MAX 4
-// #define MIN 0
 #define DIRT 'd'
 
 /*
@@ -18,51 +16,26 @@ using namespace std;
 */
 
 typedef tuple<int, int> Position;
-typedef vector<Position> Dirt;
 
 
 void next_move(int posr, int posc, vector <string> board) {
-    /* Naive Approach
-    if(posr%2==0){
-        if(board[posr][posc] == 'd') cout << "CLEAN" << endl;
-        else if(posc == MAX) cout << "DOWN" << endl;
-        else cout << "RIGHT" << endl;
-    }
-    else{
-        if(board[posr][posc] == 'd') cout << "CLEAN" << endl;
-        else if(posc == MIN) cout << "DOWN" << endl;
-        else cout << "LEFT" << endl;
-    }
-    */
     
     if(board[posr][posc] == DIRT){
         cout << "CLEAN" << endl;
         return;
     }
     
-    Dirt dirt;
+    Position dirt;
     for(int row=0; row < N; row++){
         for(int col=0; col < N; col++){
             if(board[row][col] == DIRT){
-                dirt.push_back({row, col});
+                dirt = {row, col};
             }
         }
     }
     
-    Position closest_dirt;
-    float shortest_distance = 999999;
-    for(auto d : dirt){
-        float drow = get<0>(d) - posr;
-        float dcol = get<1>(d) - posc;
-        float distance = hypot(drow, dcol);
-        if(distance < shortest_distance){
-            shortest_distance = distance;
-            closest_dirt = d;
-        }
-    }
-    
-    int drow = get<0>(closest_dirt) - posr;
-    int dcol = get<1>(closest_dirt) - posc;
+    int drow = get<0>(dirt) - posr;
+    int dcol = get<1>(dirt) - posc;
     
     if(dcol <= -1) cout << "LEFT" << endl;
     else if(dcol >= 1) cout << "RIGHT" << endl;
