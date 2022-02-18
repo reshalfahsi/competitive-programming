caa77077991046fd8bdadc9395583b1573876a47
reshalfahsi <resha.alfahsi@gmail.com>
2022-02-18 22:50:03 +0700

    update Pangrams

---
+++ go/Algorithms/Pangrams/pangrams.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strings"
+)
+
+func pangrams(s string) string {
+    lookup := make(map[rune]int)
+    for _, str := range s{
+        temp := int(str)
+        if temp > 90 { temp -= 32}
+        fmt.Println(str)
+        str = rune(byte(temp))
+        if str != ' '{
+            lookup[str] = 1
+        }
+    }
+    retval := "pangram"
+    if len(lookup) < 26 { retval = "not pangram"}
+    return retval
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    s := readLine(reader)
+
+    result := pangrams(s)
+
+    fmt.Fprintf(writer, "%s\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
